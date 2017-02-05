61aba9a4c7117e86392305825874f5fad09a6a36
sev3ryn <smlisovsky@gmail.com>
2017-02-05 16:00:24 +0100

    added handling of parenteses

--- parse/parse.go
+++ parse/parse.go
@@ -1,6 +1,7 @@
 package parse
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/sev3ryn/aritmo/scan"
@@ -25,10 +26,39 @@ func (p *Parser) next() scan.Item {
 	return p.tokens[0]
 }
 
+func (p *Parser) getOperand(tok scan.Item) float64 {
+	if tok.Typ != scan.ItemLParen {
+		return getVal(tok)
+	}
+
+	p.next()
+	return p.exec()
+
+}
+
+type operation func(float64, float64) float64
+
+func (p *Parser) getOpationFn(i scan.Item) (operation, int) {
+	switch i.Typ {
+	case scan.ItemAdd:
+		return func(a, b float64) float64 { return a + b }, 1
+	case scan.ItemSub:
+		return func(a, b float64) float64 { return a - b }, 1
+	case scan.ItemMul:
+		return func(a, b float64) float64 { return a * b }, 10
+	case scan.ItemDiv:
+		return func(a, b float64) float64 { return a / b }, 10
+	case scan.ItemRParen, scan.ItemEOF:
+		return nil, 0
+	default:
+		panic("yo")
+	}
+}
+
 func (p *Parser) exec2(valLeft float64, op operation, pr int) float64 {
 
-	tmpLeft := getVal(p.next())
-	f, nextPr := getOpFunc(p.next())
+	tmpLeft := p.getOperand(p.next())
+	f, nextPr := p.getOpationFn(p.next())
 
 	if f == nil {
 		return op(valLeft, tmpLeft)
@@ -47,30 +77,24 @@ func getVal(i scan.Item) float64 {
 	return v
 }
 
-type operation func(float64, float64) float64
-
-func getOpFunc(i scan.Item) (operation, int) {
-	switch i.Typ {
-	case scan.ItemAdd:
-		return func(a, b float64) float64 { return a + b }, 1
-	case scan.ItemSub:
-		return func(a, b float64) float64 { return a - b }, 1
-	case scan.ItemMul:
-		return func(a, b float64) float64 { return a * b }, 10
-	case scan.ItemDiv:
-		return func(a, b float64) float64 { return a / b }, 10
-	case scan.ItemEOF:
-		return nil, 0
-	default:
-		panic("yo")
+func (p *Parser) exe() float64 {
+	v := p.getOperand(p.peek())
+	f, pr := p.getOpationFn(p.next())
+	if f == nil {
+		return v
 	}
-}
-
-func (p *Parser) exec() float64 {
-	v := getVal(p.peek())
-	f, pr := getOpFunc(p.next())
 	return p.exec2(v, f, pr)
 
+}
+
+var i int
+
+func (p *Parser) exec() float64 {
+	i++
+	z := i
+	v := p.exe()
+	fmt.Printf("%d: %f\n", z, v)
+	return v
 }
 
 // func (p *Parser) exec() float64 {
