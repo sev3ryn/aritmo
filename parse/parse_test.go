740f3db7eb25e4b08fb2d32ba1744d42fbe2ea41
sev3ryn <smlisovsky@gmail.com>
2017-02-05 15:09:11 +0100

    improved parse tests

--- parse/parse_test.go
+++ parse/parse_test.go
@@ -6,18 +6,31 @@ import (
 	"github.com/sev3ryn/aritmo/scan"
 )
 
+type parseTest struct {
+	input  string
+	result float64
+}
+
+var parseTests = []parseTest{
+	{"1+2+3+4", 10},
+	{"1+2*3+4", 11},
+	{"1+2*(3+4)", 15},
+	{"1+2*(3+4)-5", 10},
+	{"1+2*(3+4)-5*6", -15},
+	{"1+2*(3+4)-5*(6+7)", -50},
+	//{"1+2*((3+4))", 15},
+	//{"1+2*((3+4)*5+6)", 83},
+}
+
 func TestParser(t *testing.T) {
 
-	s := scan.New("1+2+3+4")
-	p := New(s)
-	if res := p.exec(); res != 10 {
-		t.Errorf("1+2+3+4=%d instead of 10", res)
+	for _, tst := range parseTests {
+		t.Run(tst.input, func(t *testing.T) {
+			s := scan.New(tst.input)
+			p := New(s)
+			if res := p.exec(); res != tst.result {
+				t.Errorf("1+2+3+4=%f instead of %f", res, tst.result)
+			}
+		})
 	}
-
-	s = scan.New("1+2*3+4")
-	p = New(s)
-	if res := p.exec(); res != 11 {
-		t.Errorf("1+2*3+4=%d instead of 11", res)
-	}
-
 }
