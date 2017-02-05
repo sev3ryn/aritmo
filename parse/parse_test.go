e222206c46d9b68f6ad71b2781df1c11b71bed6c
sev3ryn <smlisovsky@gmail.com>
2017-02-05 15:23:11 +0100

    fixed error string in output

--- parse/parse_test.go
+++ parse/parse_test.go
@@ -15,10 +15,11 @@ var parseTests = []parseTest{
 	{"1+2+3+4", 10},
 	{"1+2*3+4", 11},
 	{"1+2*(3+4)", 15},
-	{"1+2*(3+4)-5", 10},
+	{"1+2*(3+4)-5", 5},
 	{"1+2*(3+4)-5*6", -15},
 	{"1+2*(3+4)-5*(6+7)", -50},
-	//{"1+2*((3+4))", 15},
+	// {"1+2*((3+4))", 15},
+	// {"1+2*(3+4)-(5)", 10},
 	//{"1+2*((3+4)*5+6)", 83},
 }
 
@@ -29,7 +30,7 @@ func TestParser(t *testing.T) {
 			s := scan.New(tst.input)
 			p := New(s)
 			if res := p.exec(); res != tst.result {
-				t.Errorf("1+2+3+4=%f instead of %f", res, tst.result)
+				t.Errorf("%s=%f instead of %f", tst.input, res, tst.result)
 			}
 		})
 	}
