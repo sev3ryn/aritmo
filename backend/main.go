83e901ac09b2e3db03e727ce324340194b08fabe
sev3ryn <smlisovsky@gmail.com>
2017-03-02 17:29:52 +0100

    added simple backend

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("../frontend/js/")))
+	http.ListenAndServe(":3000", r)
 }
